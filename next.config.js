d81a7d4ffc38ddb7fe5e65a6afa64b4334b4b122
Alejandro <alejandroaperez1994g@gmail.com>
2022-11-05 00:05:27 +0100

    feat: :sparkles: added config files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      'media4.giphy.com',
+      'media1.giphy.com',
+      'media3.giphy.com',
+      'media0.giphy.com',
+      'media2.giphy.com',
+      'media5.giphy.com',
+      'media.giphy.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
