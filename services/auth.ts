7af8c7619180e700c74013c7bcaad1e409086e60
Alejandro <alejandroaperez1994g@gmail.com>
2022-11-07 10:08:42 +0100

    fix: :bug: fix type error

--- services/auth.ts
+++ services/auth.ts
@@ -1,7 +1,11 @@
 import { provider, auth } from '../config/firebase';
 import { signInWithPopup, UserCredential } from 'firebase/auth';
 
-export const authUser = async (): Promise<UserCredential> => {
+interface ErrorResponse {
+  error: string;
+}
+
+export const authUser = async (): Promise<UserCredential | ErrorResponse> => {
   try {
     const response = await signInWithPopup(auth, provider);
     return response;
